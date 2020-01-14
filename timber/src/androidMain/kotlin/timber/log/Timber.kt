11361c9811b516b60a5621efb4906bfaafac5f8d
Merthan <me2@live.de>
2020-01-14 20:55:19 +0100

    Added the HyperlinkedDebugTree
    It creates logs that include hyperlinks to the calling line and class.
    This way you can just click on the Tag and directly jump to the source. Very useful in big projects.

--- timber/src/androidMain/kotlin/timber/log/Timber.kt
+++ timber/src/androidMain/kotlin/timber/log/Timber.kt
@@ -276,7 +276,18 @@ actual class Timber actual private constructor() {
     }
   }
 
-  actual companion object Forest : Tree() {
+  /**
+   * A [Tree] for debug builds.
+   * Automatically shows a Hyperlink to the calling Class and Linenumber in the Logs.
+   * Allows quick lookup of the caller source just by clicking on the Hyperlink in the Log.
+   * @param showMethodName Whether or not to show the method name as well
+   */
+  class HyperlinkedDebugTree(private val showMethodName: Boolean = true) : DebugTree() {
+    override fun createStackElementTag(element: StackTraceElement) =
+            with(element) { "($fileName:$lineNumber) ${if (showMethodName) " $methodName()" else ""}" }
+  }
+
+actual companion object Forest : Tree() {
     /** Log a verbose message with optional format args. */
     @JvmStatic override fun v(@NonNls message: String?, vararg args: Any?) {
       treeArray.forEach { it.v(message, *args) }
