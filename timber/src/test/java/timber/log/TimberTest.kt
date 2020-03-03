50973555d38d2451c857a04961405d98741b3ce6
Hannes Achleitner <hannes.achleitner@partner.bmw.de>
2020-03-03 18:37:42 +0100

    log with arguments

--- timber/src/test/java/timber/log/TimberTest.kt
+++ timber/src/test/java/timber/log/TimberTest.kt
@@ -276,7 +276,7 @@ class TimberTest {
     Timber.plant(object : Timber.DebugTree() {
       private val MAX_TAG_LENGTH = 23
 
-      override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
+      override fun logMessage(priority: Int, tag: String?, message: String, t: Throwable?, vararg args: Any?) {
         try {
           assertTrue(Log.isLoggable(tag, priority))
           if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N) {
@@ -286,7 +286,7 @@ class TimberTest {
           fail(e.message)
         }
 
-        super.log(priority, tag, message, t)
+        super.logMessage(priority, tag, message, t)
       }
     })
     ClassNameThatIsReallyReallyReallyLong()
@@ -402,7 +402,7 @@ class TimberTest {
   @Test fun logMessageCallback() {
     val logs = ArrayList<String>()
     Timber.plant(object : Timber.DebugTree() {
-      override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
+      override fun logMessage(priority: Int, tag: String?, message: String, t: Throwable?, vararg args: Any?) {
         logs.add("$priority $tag $message")
       }
     })
@@ -595,7 +595,7 @@ class TimberTest {
     throw AssertionError("Expected body to throw ${T::class.java.name} but completed successfully")
   }
 
-  private class LogAssert internal constructor(private val items: List<LogItem>) {
+  private class LogAssert(private val items: List<LogItem>) {
     private var index = 0
 
     fun hasVerboseMessage(tag: String, message: String): LogAssert {
